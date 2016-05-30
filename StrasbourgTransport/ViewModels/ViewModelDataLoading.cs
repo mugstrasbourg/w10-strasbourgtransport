2b8c3edc3dd106824ad77bdc1a4d2da1bcce7630
arnau <arnau@LAPTOP-ARNAUD>
2016-05-30 23:17:24 +0200

    Mise à jour des packages et ajout du ViewModelDataLoading manquant

---
+++ StrasbourgTransport/ViewModels/ViewModelDataLoading.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace StrasbourgTransport.ViewModels
+{
+    public class ViewModelDataLoading : ViewModelBase
+    {
+        private bool _isDataLoading;
+        public bool IsDataLoading
+        {
+            get
+            {
+                return _isDataLoading;
+            }
+            protected set
+            {
+                if (value != _isDataLoading)
+                {
+                    _isDataLoading = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+    }
+}
